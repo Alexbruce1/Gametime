0bf10586d321a1b45ee67a92bef05bde7519b659
Alex Bruce <brucealmightyalex@yahoo.com>
2018-08-16 08:50:37 -0600

    Add game info, like score, lives, gameOver, and rank

---
+++ test/Game-test.js
@@ -0,0 +1,16 @@
+const { assert } = require('chai');
+
+const ctx = {
+  canvas: {
+    width: 300,
+    height: 300
+  }
+};
+
+describe('Game', () => {
+  it('should take properties', () => { });
+  it('should end game', () => { });
+  it('should collide with walls', () => { });
+  it('should be able to move', () => { });
+  it('should be able to changeDirection', () => { });
+});
